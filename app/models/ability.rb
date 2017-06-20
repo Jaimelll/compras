2a650c865ff4aba391dd549a14b853d3d0f1935f
Jaimelll <jaduran0102@gmail.com>
2017-06-20 11:18:44 -0500

    exped

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,7 +13,7 @@ class Ability
           can :read, ActiveAdmin::Page, :name =>"Dashboard"
           can [:read], [Item, Detail]
           can [:read], [Phase,Activity,Piece]
-      when 10,5,16,18,19 #roy,asesor,ballesteros
+      when 10,5,16,18,19,20 #roy,asesor,ballesteros
         can :read, ActiveAdmin::Page, :name =>"Dashboard"
         can [:read], [Item, Detail]
     #   when 5 # ballesteros
