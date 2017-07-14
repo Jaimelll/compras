3daa5ccf299b2508e0a322a2bfaabf470168bb5c
Jaimelll <jaduran0102@gmail.com>
2017-07-14 11:04:48 -0500

    exped

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,12 @@ class Item < ApplicationRecord
   has_many :details
 
 
-  validates :obac, presence: true
-  validates :lista, presence: true
-  validates :modalidad, presence: true
-  validates :exped2, presence: true
-  validates :ejecucion, presence: true
-  validates :tipo, presence: true
+#  validates :obac, presence: true
+#  validates :lista, presence: true
+#  validates :modalidad, presence: true
+#  validates :exped2, presence: true
+#  validates :ejecucion, presence: true
+#  validates :tipo, presence: true
 
 
 
