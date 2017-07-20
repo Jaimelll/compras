22f00a3ff4c0a16a4148d931e5c34399ca977f70
Jaimelll <jaduran0102@gmail.com>
2017-07-20 17:47:59 -0500

    exped

--- app/admin/activity.rb
+++ app/admin/activity.rb
@@ -196,7 +196,7 @@ form :title => 'Edicion Actividad'  do |f|
                     if activity.plan then
                        activity.plan.strftime("%d-%m-%Y")
                     else
-                      Activity.where( phase_id:activity.phase_id).update_all( plan:activity.pfecha )
+                    #  Activity.where( phase_id:activity.phase_id).update_all( plan:activity.pfecha )
                       "s/d"
                      end
                    end
