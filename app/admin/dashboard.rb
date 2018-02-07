35d4f30d3455a10f1047d6c132573408668c8999
Jaimelll <jaduran0102@gmail.com>
2018-02-07 14:41:40 -0500

    encargo

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -224,6 +224,7 @@ end
                       #  li link_to "Personal por area evaluacion", reports_vhoja4_path(format: "xlsx")
                       #  li    link_to "Personal", "https://secure-harbor-85875.herokuapp.com"
                        li    link_to "Personal", "http://172.25.10.6:3001/admin/login"
+                       li    link_to "Hoja", "C:\Users\AUXILIAR.PROCESOS\Downloads\Actos.xlsx"
                       end #de panel historial periodos
 
 ###############################
