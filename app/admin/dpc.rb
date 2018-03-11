34a8ffc54ebc33df00c10befaca1dbe7e9069327
Jaimelll <jaduran0102@gmail.com>
2018-03-11 18:42:19 -0500

    detalle subtotal

--- app/admin/dpc.rb
+++ app/admin/dpc.rb
@@ -70,7 +70,7 @@ ActiveAdmin.register_page "Dpc" do
 
 
 
-      
+
 
 
 #################
@@ -427,7 +427,7 @@ column do
 
                      if proceso.moneda and proceso.valor then
 
-                          @vpv=Formula.where(product_id:7,orden:proceso.moneda)
+                          @vpv=Formula.where(product_id:7,orden:proceso.moneda,numero:@vaf)
                                 .select('cantidad as dd').first.dd.to_i*proceso.valor/100
 
 
@@ -448,7 +448,7 @@ column do
                       @vxper3[2]=@vxper3[2]+1
                       if proceso.valor then
 
-                         @vpv=Formula.where(product_id:7,orden:proceso.moneda)
+                         @vpv=Formula.where(product_id:7,orden:proceso.moneda,numero:@vaf)
                                .select('cantidad as dd').first.dd.to_i*proceso.valor/100
 
                          @contavus[2]=  @contavus[2]+@vpv
@@ -500,7 +500,7 @@ column do
 
                        if proceso.moneda and proceso.valor then
 
-                           @vpv=Formula.where(product_id:7,orden:proceso.moneda,numero:proceso.periodo)
+                           @vpv=Formula.where(product_id:7,orden:proceso.moneda,numero:@vaf)
                                 .select('cantidad as dd').first.dd.to_i*proceso.valor/100
 
 
@@ -526,7 +526,7 @@ column do
                              where("adjudicado IS NOT NULL and moneda IS NOT NULL  and (estado=4 or estado=9)").count>0  then
                                  Piece.where(phase_id:proceso.id).
                                  where("adjudicado IS NOT NULL and moneda IS NOT NULL  and (estado=4 or estado=9)").each do |adju|
-                              @vpv=@vpv+adju.adjudicado*Formula.where(product_id:7,orden:adju.moneda,numero:proceso.periodo)
+                              @vpv=@vpv+adju.adjudicado*Formula.where(product_id:7,orden:adju.moneda,numero:@vaf)
                               .select('cantidad as dd').first.dd.to_i/100
                           end
                           end
