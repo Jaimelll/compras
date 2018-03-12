6a44390c956cdb9c14acd6c4350c812fc0133b89
Jaimelll <jaduran0102@gmail.com>
2018-03-12 14:56:04 -0500

    encargo

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -188,12 +188,18 @@ end
    #loca1='//W:/SG ACFFAA/Oenlace/Proyectos/'
    #loca1='\\acffaa-archivo\acffaa$\SG ACFFAA\Oenlace\Proyectos\ '
 #  localiz='W:\SG ACFFAA\Oenlace\Proyectos\PACFAP038COLCHONES.mpp'
+# vlink='PACFAP038COLCHONES'
+# localiz=loca1+vlink+'.mpp'
+#link_to item.descripcion,localiz, :target=>"_blank"
+#  item.descripcion
+
+
+
   localiz='file:///W:/SG%20ACFFAA/Oenlace/Proyectos/H5.mpp'
 
-  # vlink='PACFAP038COLCHONES'
-  # localiz=loca1+vlink+'.mpp'
-   link_to item.descripcion,localiz, :target=>"_blank"
-#  item.descripcion
+
+link_to item.descripcion, root_path << localiz
+
   end
 
   column("periodo")do |item|
